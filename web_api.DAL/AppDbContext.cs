a88e32f1c8138957b99d6d3fb2ea634b0d80ef0d
Bogdan Kurylo <lo067803@outlook.com>
2025-06-30 04:03:12 +0300

    Добавьте файлы проекта.

---
+++ web_api.DAL/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace web_api.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+    }
+}
